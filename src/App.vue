651495bd9003bef6f3b4112d0b274acaaa62c366
mariamberthe214-collab <mariamberthe214@gmail.com>
2025-10-16 16:43:49 +0000

    mis a jour

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ImportPage />
   </div>
+  
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import ImportPage from './components/pages/ImportPage.vue'
 
 
 
@@ -14,6 +17,7 @@ export default {
   name: 'App',
   components: {
     HelloWorld,
+    ImportPage
   
   }
 }
